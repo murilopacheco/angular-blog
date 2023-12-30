4ba78076bde9d8cee77196e428f9911aefa8ff32
murilo <muriloredes@gmail.com>
2023-12-30 18:20:09 -0300

    Ajustes nos icones e links dos items, melhorias do layout geral, e atualização de conteúdo.

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -10,5 +10,17 @@ export const dataFake = [
     "title": "Nova Série anunciada no Disney +",
     "description": "bla blabla",
     "photoCover":"https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg"
+  },
+  {
+    "id":"3",
+    "title": "Que séries e filmes estão bombando? Veja o Top 25 do Disney+ e Star+",
+    "description": "Nesta semana no Disney+, a nova série Percy Jackson e os Olimpianos continua a dominar o topo da lista como o conteúdo mais assistido. A série, aguardada com grande expectativa pelos fãs dos livros de Rick Riordan, segue em lançamento de episódios semanais até o final de janeiro.",
+    "photoCover":"https://disneyplusbrasil.com.br/wp-content/uploads/2023/12/As-Cronicas-de-Narnia.jpg"
+  },
+  {
+    "id":"4",
+    "title": "Sem aviso, Disney+ remove mais um filme do Homem-Aranha",
+    "description": "Às 2:00 AM deste sábado (30), identificamos a remoção de mais um filme de sucesso do catálogo do Disney+. Desta vez, o título excluído foi O Espetacular Homem-Aranha (2012), estrelado por Andrew Garfield. A ausência de um aviso prévio da plataforma é uma prática recorrente, deixando os fãs desapontados e sem a chance de planejar",
+    "photoCover":"https://disneyplusbrasil.com.br/wp-content/uploads/2023/12/O-Espetacular-Homem-Aranha.jpg"
   }
 ]
